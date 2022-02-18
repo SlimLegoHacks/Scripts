9d99a8ad44f9abd6f4d746c5650f21bd0fc95a90
SlimLegoHacks <99925595+SlimLegoHacks@users.noreply.github.com>
2022-02-18 14:32:30 -0500

    Fixed again :pig:

--- Main.lua
+++ Main.lua
@@ -1,8 +1,8 @@
 local checkHWID = loadstring(game:HttpGet("https://pastebin.com/raw/k2K901C5"))()
 local userHWID = game:GetService("RbxAnalyticsService"):GetClientId()
 
-for i, v in pairs(checkHWID)
-    if v == userHWID then
+for i, v in pairs(checkHWID) do
+    if v == userHWID then 
         if game.GameId == 3027710332 then 
             loadstring(game:HttpGet("https://raw.githubusercontent.com/SlimLegoHacks/Scripts/main/Budokai.lua"))()
         end
