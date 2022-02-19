7170cce95065fb78505b409e15ddbfa27531e4eb
SlimLegoHacks <99925595+SlimLegoHacks@users.noreply.github.com>
2022-02-18 20:19:45 -0500

    ABA XD NIGHGA BALSL

--- Main.lua
+++ Main.lua
@@ -5,6 +5,8 @@ for i, v in pairs(checkHWID) do
     if v == userHWID then 
         if game.GameId == 3027710332 then 
             loadstring(game:HttpGet("https://raw.githubusercontent.com/SlimLegoHacks/Scripts/main/Budokai.lua"))()
+        elseif game.GameId == 578392296 then
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/SlimLegoHacks/Scripts/main/LSOWDHUBXDNIGGABALLS.lua"))()
         else
             print('Game Not Supported')
         end
