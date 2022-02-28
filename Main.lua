b0c515bd0bf3e87735203510963aad50aaf2d222
SlimLegoHacks <99925595+SlimLegoHacks@users.noreply.github.com>
2022-02-28 13:00:44 -0500

    Universal AntiAFK, for all games

--- Main.lua
+++ Main.lua
@@ -1,6 +1,12 @@
 local checkHWID = loadstring(game:HttpGet("https://pastebin.com/raw/k2K901C5"))()
 local userHWID = game:GetService("RbxAnalyticsService"):GetClientId()
 
+local VirtualUser = game:service'VirtualUser'
+game:service'Players'.LocalPlayer.Idled:connect(function()
+VirtualUser:CaptureController()
+VirtualUser:ClickButton2(Vector2.new())
+end)
+
 for i, v in pairs(checkHWID) do
     if v == userHWID then 
         if game.GameId == 3027710332 then 
